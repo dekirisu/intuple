35068aeea8d4af59a5e8653829670e533e57a9e7
Dekirisu <dekirisu@gmail.com>
2023-11-07 16:28:49 +0100

    🐁 chase major refactor

--- tests/variants.rs
+++ tests/variants.rs
@@ -39,24 +39,6 @@ use intuple::*;
         fn var_itp_unnamed_from_tuple() {
             let a = Unnamed::<u16>::from_tuple((3,2,1));
             assert_eq!(a, Unnamed::<u16>(3,2,1));
-            let a = Unnamed::<u16>::fruple((3,2,1));
-            assert_eq!(a, Unnamed::<u16>(3,2,1));
-        }
-
-        #[test]
-        fn var_itp_tuple_into_unnamed() {
-            let a: Unnamed::<u16> = (3,2,1).into_struct();
-            assert_eq!(a, Unnamed::<u16>(3,2,1));
-            let a: Unnamed::<u16> = (3,2,1).intruct();
-            assert_eq!(a, Unnamed::<u16>(3,2,1));
-        }
-
-        #[test]
-        fn var_itp_tuple_from_unnamed() {
-            let a = <(u32, u32, u16)>::from_struct(Unnamed::<u16>(3,2,1));
-            assert_eq!(a, (3,2,1));
-            let a = <(u32, u32, u16)>::fruct(Unnamed::<u16>(3,2,1));
-            assert_eq!(a, (3,2,1));
         }
 
         #[test]
@@ -106,24 +88,6 @@ use intuple::*;
         fn var_itp_named_from_tuple() {
             let a = Named::<u16>::from_tuple((3,2,1));
             assert_eq!(a, Named{a:3,b:2,c:1});
-            let a = Named::<u16>::fruple((3,2,1));
-            assert_eq!(a, Named{a:3,b:2,c:1});
-        }
-
-        #[test]
-        fn var_itp_tuple_into_named() {
-            let a: Named::<u16> = (3,2,1).into_struct();
-            assert_eq!(a, Named::<u16>{a:3,b:2,c:1});
-            let a: Named::<u16> = (3,2,1).intruct();
-            assert_eq!(a, Named::<u16>{a:3,b:2,c:1});
-        }
-
-        #[test]
-        fn var_itp_tuple_from_named() {
-            let a = <(u32, u32, u16)>::from_struct(Named::<u16>{a:3,b:2,c:1});
-            assert_eq!(a, (3,2,1));
-            let a = <(u32, u32, u16)>::fruct(Named::<u16>{a:3,b:2,c:1});
-            assert_eq!(a, (3,2,1));
         }
 
         #[test]
